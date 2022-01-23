f20cefe5e6b4eb5cb83799ea1b0bbdc8d6cc2f11
dsvvsai3 <82382565+dsvvsai3@users.noreply.github.com>
2022-01-23 13:16:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,34 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "192.168.0.0/24"
 }
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.tamops.location
+  resource_group_name = azurerm_resource_group.tamops.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
